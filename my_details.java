a14004a3c796dab445b240e28344f9cc3924f8e8
amoakogladys200-byte <amoakogladys200@gmail.com>
2026-06-24 21:01:30 +0000

    Add files via upload

---
+++ my_details.java
@@ -0,0 +1,27 @@
+public class my_details {
+    public static void main(String[] args) {
+
+
+        // Variables
+        String name = "Amoako Gladys";
+        int age = 19;
+        String occupation = "Student";
+        long accountBalance = 234567979L;
+        int shoeSize =39; //in feet                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       42;
+        double height = 5.2; // in feet
+        String hobby = "Football";
+
+
+        // Output
+        System.out.printf("Name:Amoako Gladys%n", name);
+        System.out.printf("Age:19%n", age);
+        System.out.printf("Occupation:Student%n", occupation);
+        System.out.printf("23456779%nL", accountBalance);
+        System.out.printf("Shoe Size: 39%n", shoeSize);
+        System.out.printf("Height:5.2%n", height);
+        System.out.printf("Hobby:Football%n", hobby);
+
+
+
+    }
+}
